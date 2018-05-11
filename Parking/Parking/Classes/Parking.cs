41f7a80cc7324464a7569a0ee44d56393e7f5fac
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-05-11 22:20:21 +0300

    Create project

---
+++ Parking/Parking/Classes/Parking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Classes
+{
+    public sealed class Parking
+    {
+        private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
+
+        public static Parking Instance { get { return lazy.Value; } }
+
+        private Parking()
+        {
+
+        }
+    }
+}
