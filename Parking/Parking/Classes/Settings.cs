c9f08f8579862743ddfaea7a0d322d29b4b0ef62
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-05-11 22:27:27 +0300

    Create project

---
+++ Parking/Parking/Classes/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Classes
+{
+    class Settings
+    {
+        Dictionary<CarType, int> prices;
+        public int TimeOut { get; set; }
+        public int ParkingSpace { get; set; }
+        public double Fine { get; set; }
+
+        public Settings()
+        {
+            prices = new Dictionary<CarType, int>()
+            {
+                { CarType.Truck, 5 },
+                { CarType.Passenger, 3 },
+                { CarType.Bus, 2 },
+                { CarType.Motorcycle, 1 }
+            };
+            TimeOut = 3;
+            ParkingSpace = 50;
+            Fine = 1.1;
+        }
+
+        public void FileReader()
+        {
+
+        }
+    }
+}
